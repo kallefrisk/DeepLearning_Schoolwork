c994723690fe5666f80526712ecd6e38ad4e4220
kallefrisk <kallefrisk@live.se>
2026-05-18 15:14:51 +0200

    Added GRU-model.

--- KarlProject/notebooks/RNN_models.py
+++ KarlProject/notebooks/RNN_models.py
@@ -65,3 +65,35 @@ class LSTMModel(nn.Module):
 
         output = self.fc(last_out)
         return output
+
+
+# 3. GRU MODEL
+class GRUModel(nn.Module):
+    """
+    GRU model for sequence classification.
+    """
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout=0.0):
+        super(GRUModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.gru = nn.GRU(input_size, hidden_size, num_layers,
+                          batch_first=True, dropout=dropout)
+
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        """
+        x shape: (batch, seq_len, input_size)
+        Returns:
+            out_shape:
+        """
+
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.gru(x, h0)
+
+        last_out = out[:, -1, :]  # (batch, hidden_size)
+
+        output = self.fc(last_out)
+        return output
