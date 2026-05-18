0c54f2e1251429d301b40b762467c7ffdc43b2f6
kallefrisk <kallefrisk@live.se>
2026-05-18 15:11:17 +0200

    Added LSTM-model.

--- KarlProject/notebooks/RNN_models.py
+++ KarlProject/notebooks/RNN_models.py
@@ -32,3 +32,36 @@ class SimpleRNNModel(nn.Module):
 
         output = self.fc(last_out)
         return output
+
+
+# 2. LSTM MODEL
+class LSTMModel(nn.Module):
+    """
+    LSTM model for sequence classification.
+    """
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout=0.0):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers,
+                            batch_first=True, dropout=dropout)
+
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        """
+        x shape: (batch, seq_len, input_size)
+        Returns:
+            out_shape:
+        """
+
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+
+        last_out = out[:, -1, :]  # (batch, hidden_size)
+
+        output = self.fc(last_out)
+        return output
