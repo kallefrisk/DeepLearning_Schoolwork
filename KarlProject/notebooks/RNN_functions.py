8b9037115721702e1205379f323589c292fc3a59
kallefrisk <kallefrisk@live.se>
2026-05-24 17:48:52 +0200

    Generalized the loading function to scale any input comun instead.

--- KarlProject/notebooks/RNN_functions.py
+++ KarlProject/notebooks/RNN_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 
-def load_video_score(score_path: str = None, lower_bound: float = 0, upper_bound: float = 4) -> pd.DataFrame:
+def load_video_score(score_path: str = None, lower_bound: float = 0, upper_bound: float = 4, columns: list[str] = ["score"]) -> pd.DataFrame:
     """
     Loads and scales the video score found in the target file into the range [lower_bound, upper_bound]
 
@@ -10,25 +10,28 @@ def load_video_score(score_path: str = None, lower_bound: float = 0, upper_bound
         score_path: path the the csv-file containing the score column
         lower_bound: The lower bound of the output range
         upper_bound: The upper bound of the output range
+        columns: The columns to scale
 
     Returns:
-        DataFrame: A pandas DataFrame with the scaled scores fit to the input range using MinMaxScaler from sklearn as the last column.
+        DataFrame: A pandas DataFrame with the scaled columns fit to the input range using MinMaxScaler from sklearn as the last column.
     """
 
     if score_path is None:
         raise Exception("Path cannot be of type 'None'")
 
-    scores = pd.read_csv(score_path)
+    df = pd.read_csv(score_path)
 
-    if "score" in scores.columns:
-        # Remove faulty scores
-        scores = scores.loc[scores["score"] != 0.0]
+    for column in columns:
+        if column in df.columns:
+            if column == "score":
+                # Remove faulty scores
+                df = df.loc[df["score"] != 0.0]
 
-        scaler = MinMaxScaler(feature_range=(lower_bound, upper_bound))
-        scores["scaled_score"] = scaler.fit_transform(scores[["score"]])
-        return scores
+            scaler = MinMaxScaler(feature_range=(lower_bound, upper_bound))
+            df[f"scaled_{column}"] = scaler.fit_transform(df[[column]])
+        return df
     else:
-        raise Exception("File has no column named 'score'")
+        raise Exception(f"File has no column named '{column}'")
 
 
 # Test if the functions work
